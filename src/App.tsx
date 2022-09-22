3670fb322f938a41969bb4429ac4a55b3243e2d9
marciovz <marciovz@yahoo.com.br>
2022-09-22 10:59:59 -0300

    instalação e configuração base do styled-component e tema default

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <p>Home - Movie Store</p>
-    </>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  white: '#ffffff',
+  background: '#fafafa',
+
+  'base-title': '#272221',
+  'base-text': '#574f4d',
+}
